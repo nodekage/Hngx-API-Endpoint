dfd353b7d0c8996c7711c4d5e17b92fe5d4f269e
nodekage <anthonycuzoho@gmail.com>
2023-09-07 15:31:33 +0100

    feat : API setup

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 4000; 
+
+app.get('/api', (req, res) => {
+  // Query parameters
+  const { slack_name, track } = req.query;
+
+  // Day of the week
+  const currentDay = new Date().toLocaleString('en-US', { weekday: 'long' });
+
+  // UTC time within a +/-2 minute window
+  const currentTime = new Date(Date.now() - 120000).toISOString();
+
+  // JSON response
+  const response = {
+    slack_name: slack_name,
+    current_day: currentDay,
+    utc_time: currentTime,
+    track: track,
+    github_file_url: 'https://github.com/username/repo/blob/main/file_name.ext',
+    github_repo_url: 'https://github.com/username/repo',
+    status_code: 200,
+  };
+
+  res.status(200).json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
